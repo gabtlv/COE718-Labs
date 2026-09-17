749df6995c1f7aba41b6b5aba962289fd4f19384
gab <156394839+gabtlv@users.noreply.github.com>
2026-09-17 15:46:28 -0400

    Refactor LCD handling and joystick direction display
    Updated LCD initialization and display functions. Improved joystick direction display handling.

--- Lab1/Boards/Keil/MCB1700/Blinky_ULp/Blinky.c
+++ Lab1/Boards/Keil/MCB1700/Blinky_ULp/Blinky.c
@@ -3,27 +3,30 @@
 #include "Board_ADC.h"
 #include "KBD.h"
 
-#define _USE_LCD           // comment this out when running in Debug/simulation mode
+#define _USE_LCD
 #ifdef _USE_LCD
   #include "GLCD.h"
 #endif
 
 int main (void) {
   uint32_t joy;
-  uint32_t last_joy = 0xFFFFFFFF;   // forces the LCD to draw on the very first loop
+  uint32_t last_joy = 0xFFFFFFFF;
 
   LED_Init();
-  ADC_Initialize();        // still required: IRQ.c's SysTick handler still triggers
-                            // ADC conversions each tick, even though we ignore the result
+  ADC_Initialize();
   KBD_Init();
 
 #ifdef _USE_LCD
-  GLCD_Initialize();
-  GLCD_SetForegroundColor(GLCD_COLOR_WHITE);
-  GLCD_SetBackgroundColor(GLCD_COLOR_BLUE);
-  GLCD_ClearScreen();
-  GLCD_DrawString(10, 10, "COE718 Lab 1");
-  GLCD_DrawString(10, 25, "Joystick Direction Demo");
+  GLCD_Init();
+  GLCD_Clear(Blue);
+  GLCD_SetBackColor(White);
+  GLCD_SetTextColor(Black);
+  GLCD_DisplayString(0, 0, 1, (unsigned char *)"     COE718 Lab 1     ");
+  GLCD_DisplayString(1, 0, 1, (unsigned char *)"  Joystick Direction  ");
+
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(White);
+	GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: NONE");
 #endif
 
   SystemCoreClockUpdate();
@@ -32,33 +35,34 @@ int main (void) {
   while (1) {
     joy = get_button();
 
-    if (joy != last_joy) {         // only touch LEDs/LCD when direction changes
+    if (joy != last_joy) {
       last_joy = joy;
 
       if (joy & KBD_UP) {
         LED_On(0);  LED_Off(1); LED_Off(2); LED_Off(3);
 #ifdef _USE_LCD
-        GLCD_DrawString(10, 50, "Direction: UP    ");
+				GLCD_ClearLn(3, 1);
+        GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: UP     ");
 #endif
       } else if (joy & KBD_DOWN) {
         LED_Off(0); LED_On(1);  LED_Off(2); LED_Off(3);
 #ifdef _USE_LCD
-        GLCD_DrawString(10, 50, "Direction: DOWN  ");
+        GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: DOWN   ");
 #endif
       } else if (joy & KBD_LEFT) {
         LED_Off(0); LED_Off(1); LED_On(2);  LED_Off(3);
 #ifdef _USE_LCD
-        GLCD_DrawString(10, 50, "Direction: LEFT  ");
+        GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: LEFT   ");
 #endif
       } else if (joy & KBD_RIGHT) {
         LED_Off(0); LED_Off(1); LED_Off(2); LED_On(3);
 #ifdef _USE_LCD
-        GLCD_DrawString(10, 50, "Direction: RIGHT ");
+        GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: RIGHT  ");
 #endif
       } else if (joy & KBD_SELECT) {
         LED_Off(0); LED_Off(1); LED_Off(2); LED_Off(3);
 #ifdef _USE_LCD
-        GLCD_DrawString(10, 50, "Direction: SELECT");
+        GLCD_DisplayString(4, 0, 1, (unsigned char *)"Direction: SELECT ");
 #endif
       }
     }
